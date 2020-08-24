f9e7b54a5883285c9c642127f0a3b38862206e01
Emmanuel Ndayiragije <endayiragije@gmail.com>
2020-08-25 01:12:52 +0200

    Adding some chnages

--- AspNetSignalR.App/Controllers/HomeController.cs
+++ AspNetSignalR.App/Controllers/HomeController.cs
@@ -13,21 +13,7 @@ namespace AspNetSignalR.App.Controllers
 		{
 			return View();
 		}
-
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
-		}
-
+		
 		[Authorize]
 		public ActionResult Chat()
 		{
